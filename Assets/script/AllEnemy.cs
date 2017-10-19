dbc13a3dcc02066f35773d5f06030e10ff2bdc59
DESKTOP-8JCA1RT\Quintin <qriehle@mail.pima.edu>
2017-10-19 14:45:07 -0700

    made a copy of an old script to start from for new script

--- Assets/script/AllEnemy.cs
+++ Assets/script/AllEnemy.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class AllEnemy : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public float moveSpeed = 3f;
+    Transform leftWayPoint, rightWayPoint;
+    Vector3 localScale;
+    bool movingRight = true;
+    Rigidbody2D rb;
+    // Use this for initialization
+    void Start () {
+
+        localScale = transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+        leftWayPoint = GameObject.Find ("LeftWayPoint").GetComponent<Transform>();
+        rightWayPoint = GameObject.Find("RightWayPoint").GetComponent<Transform>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (transform.position.x > rightWayPoint.position.x)
+            movingRight = false;
+        if (transform.position.x < leftWayPoint.position.x)
+            movingRight = true;
+
+        if (movingRight)
+            moveRight();
+        else
+            moveLeft();
+    }
+    void moveRight()
+    {
+        movingRight = true;
+        localScale.x = Mathf.Abs( localScale.x );
+        transform.localScale = localScale;
+        rb.velocity = new Vector2(localScale.x * moveSpeed, rb.velocity.y);
+
+    }
+
+    void moveLeft()
+    {
+        movingRight = false;
+        localScale.x = -Mathf.Abs(localScale.x);
+        transform.localScale = localScale;
+        rb.velocity = new Vector2(localScale.x * moveSpeed, rb.velocity.y);
+
+    }
 }
