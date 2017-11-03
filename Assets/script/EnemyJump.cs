b269a9ad349d36c43561fdf18b7d902b020cf9b3
EDU-DOMAIN\qriehle <qriehle@mail.pima.edu>
2017-11-02 19:50:25 -0700

    Jumping Enemy script made and added to grape in prefab

--- Assets/script/EnemyJump.cs
+++ Assets/script/EnemyJump.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyJump : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class EnemyJump : Follow
+{
+    public Rigidbody2D body;
+    public float EJump = 10f;
+    public override void Update()
+    {
+        base.Update();
+            if (Input.GetButtonDown("Vertical"))
+            {
+                if (body.velocity.y < 0.1f)
+                {
+                    if (body.velocity.y >= 0f)
+                    {
+                        body.AddForce(Vector2.up * EJump, ForceMode2D.Impulse);
+                    }
+                }
+            }
+    }
 }
