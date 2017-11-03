9bc97c54e0455c07a4978518c9b153fb93135dac
ASUS-WOODS\Cody <woodscody4@gmail.com>
2017-11-02 18:05:55 -0700

    added scripts

---
+++ Assets/script/HealthScript2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript2 : MonoBehaviour {
+    public float max_health = 100f;
+    public float cur_health = 0f;
+	// Use this for initialization
+	void Start () {
+        cur_health = max_health;
+
+	}
+    public void TakeDamage(float amount)
+    {
+        cur_health -= amount;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
